8902374e32349d2e04b623a8fae03de96aacee08
Ivana Rojas <irojascorsico@gmail.com>
2020-10-19 20:19:43 -0300

    solucion inicial

---
+++ fundamentos-poo2/fundamentos-poo2/Operacion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fundamentos_poo2
+{
+    public class Operacion
+    {
+        private int valor1;
+        private int valor2;
+        private int resultado;
+
+        public int Valor1 {
+            get { return valor1; }
+            set { valor1 = value; }
+        }
+        public int Valor2
+        {
+            get { return valor2; }
+            set { valor2 = value; }
+        }
+
+        public int Resultado
+        {
+            get { return resultado; }
+            set { resultado = value; }
+        }
+    }
+
+    public class Suma:Operacion
+    {
+        public void Sumar()
+        {
+            Resultado = Valor1 + Valor2;
+        }
+    }
+}
---
+++ fundamentos-poo2/fundamentos-poo2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fundamentos_poo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Suma objSuma = new Suma();
+            objSuma.Valor1 = 2;
+            objSuma.Valor2 = 4;
+            objSuma.Sumar();
+            Console.WriteLine("El resultado de la suma es: " + objSuma.Resultado);
+        }
+    }
+}
